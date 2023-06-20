96765ba8513e2188de9b373ddc86fc5e924ddc63
torben.unland <torben.unland@gsits.de>
2023-06-20 10:08:16 +0200

    added background black and trans and made telescrope transparent for rose pine and removed colorcolumn

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,110 +1,57 @@
--- tundra
--- require('nvim-tundra').setup({
---   transparent_background = true,
---   dim_inactive_windows = {
---     enabled = false,
---     color = nil,
---   },
---   sidebars = {
---     enabled = true,
---     color = nil,
---   },
---   editor = {
---     search = {},
---     substitute = {},
---   },
---   syntax = {
---     booleans = { bold = true, italic = true },
---     comments = { bold = true, italic = true },
---     conditionals = {},
---     constants = { bold = true },
---     fields = {},
---     functions = {},
---     keywords = {},
---     loops = {},
---     numbers = { bold = true },
---     operators = { bold = true },
---     punctuation = {},
---     strings = {},
---     types = { italic = true },
---   },
---   diagnostics = {
---     errors = {},
---     warnings = {},
---     information = {},
---     hints = {},
---   },
---   plugins = {
---     lsp = true,
---     treesitter = true,
---     telescope = true,
---     nvimtree = true,
---     cmp = true,
---     context = true,
---     dbui = true,
---     gitsigns = true,
---     neogit = true,
---   },
---   overwrite = {
---     colors = {},
---     highlights = {},
---   },
--- })
-
 -- catppuccin
--- require("catppuccin").setup({
---     flavour = "mocha", -- latte, frappe, macchiato, mocha
---     background = { -- :h background
---         light = "latte",
---         dark = "mocha",
---     },
---     transparent_background = true,
---     show_end_of_buffer = false, -- show the '~' characters after the end of buffers
---     term_colors = false,
---     dim_inactive = {
---         enabled = false,
---         shade = "dark",
---         percentage = 0.15,
---     },
---     no_italic = false, -- Force no italic
---     no_bold = false, -- Force no bold
---     styles = {
---         comments = { "italic" },
---         conditionals = { "italic" },
---         loops = {},
---         functions = {},
---         keywords = {},
---         strings = {},
---         variables = {},
---         numbers = {},
---         booleans = {},
---         properties = {},
---         types = {},
---         operators = {},
---     },
---     color_overrides = {
---         all = {
---             text = "#ffffff",
---         },
---         mocha = {
---             base = "#1e1e2e",
---         },
---         frappe = {},
---
---         macchiato = {},
---         latte = {},
---     },
---     custom_highlights = {},
---     integrations = {
---         cmp = true,
---         gitsigns = true,
---         nvimtree = true,
---         telescope = true,
---         notify = false,
---         mini = false,
---         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
---     },
--- })
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = true,
+    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {
+        all = {
+            text = "#ffffff",
+        },
+        mocha = {
+            base = "#1e1e2e",
+        },
+        frappe = {},
+
+        macchiato = {},
+        latte = {},
+    },
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
 
 
 -- everfrost
@@ -127,8 +74,86 @@ require("everforest").setup({
   disable_italic_comments = false,
 })
 
+-- nord
+vim.g.nord_contrast = true
+
+vim.g.nord_borders = false
+vim.g.nord_disable_background = true 
+vim.g.nord_italic = false
+
+vim.g.nord_uniform_diff_background = true
+
+vim.g.nord_bold = false
+-- Load the colorscheme
+require('nord').set()
+
+-- rose pine
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'main',
+	--- @usage 'main'|'moon'|'dawn'
+
+	dark_variant = 'main',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = true,
+	disable_float_background = false,
+	disable_italics = false,
+
+
+	--- @usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		background = 'base',
+		background_nc = '_experimental_nc',
+		panel = 'surface',
+		panel_nc = 'base',
+		border = 'highlight_med',
+
+		comment = 'muted',
+		link = 'iris',
+
+		punctuation = 'subtle',
+
+		error = 'love',
+		hint = 'iris',
+		info = 'foam',
+		warn = 'gold',
+
+		headings = {
+			h1 = 'iris',
+			h2 = 'foam',
+			h3 = 'rose',
+
+			h4 = 'gold',
+			h5 = 'pine',
+			h6 = 'foam',
+		}
+		-- or set all headings at once
+		-- headings = 'subtle'
+	},
+
+	-- Change specific vim highlight groups
+	-- https://github.com/rose-pine/neovim/wiki/Recipes
+	highlight_groups = {
+		ColorColumn = { bg = 'rose' },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = 'foam', blend = 10 },
+		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+
+        -- transparent telescope
+        TelescopeBorder = { fg = "highlight_high", bg = "none" },
+		TelescopeNormal = { bg = "none" },
+		TelescopePromptNormal = { bg = "base" },
+		TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+		TelescopeSelection = { fg = "text", bg = "base" },
+		TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+	}
+})
+
+
 function ColorMyPencils(color)
-    color = color or "everforest"
+    color = color or "rose-pine"
     vim.opt.background = 'dark'
     vim.cmd.colorscheme(color)
 end
